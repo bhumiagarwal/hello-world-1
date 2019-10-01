b9cadeaeae08360ecf6627ca96e2b4733552a42d
rj-riyajain <55906793+rj-riyajain@users.noreply.github.com>
2019-10-01 20:11:49 +0530

    C lan 3

---
+++ 1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+/*Declaring variable*/
+char name[50], address[50];
+int enroll;
+
+/*Taking input*/
+printf("Enter your name:");
+gets(name);
+printf("Enter your enrollment number:");
+scanf("%d", &enroll);
+printf("Enter your address:");
+fflush(stdin);/*Clear whole data*/
+gets(address);
+
+/*Taking output*/
+printf("Your name is: %s\n", name);
+printf("Your enrollment number is: %d\n", enroll);
+printf("Your address is: %s", address);
+return 0;
+}
