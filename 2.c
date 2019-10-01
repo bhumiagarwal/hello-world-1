f03217cc0ac9c6ab3740983f65da84506db40897
rj-riyajain <55906793+rj-riyajain@users.noreply.github.com>
2019-10-01 20:13:46 +0530

    C lang4

---
+++ 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    /*Initialising*/
+    int i=10;
+    float f=10.00;
+    char c='a';
+
+    /*Getting output*/
+    printf("%d\n%f\n%c", i, f, c);
+    return 0;
+}
